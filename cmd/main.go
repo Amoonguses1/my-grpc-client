fef32704c554511aed14bcf4f98ced3820ba2ca8
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2024-09-21 02:23:30 +0900

    Implement error handling

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
 
+	"github.com/amoonguses1/my-grpc-client/internal/adaptor/bank"
 	"github.com/amoonguses1/my-grpc-client/internal/adaptor/hello"
+	dbank "github.com/amoonguses1/my-grpc-client/internal/application/domain/bank"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
@@ -22,15 +26,24 @@ func main() {
 	}
 	defer conn.Close()
 
-	helloAdaptor, err := hello.NewHelloAdaptor(conn)
+	// helloAdaptor, err := hello.NewHelloAdaptor(conn)
+	// if err != nil {
+	// 	log.Fatalln("Can not create HelloAdaptor :", err)
+	// }
+
+	bankAdaptor, err := bank.NewBankAdapter(conn)
 	if err != nil {
-		log.Fatalln("Can not create HelloAdaptor :", err)
+		log.Fatalln("Can not create BankAdapter :", err)
 	}
 
 	// runSayHello(helloAdaptor, "my name")
 	// runSayManyHellos(helloAdaptor, "call multiple name")
 	// runSayHelloToEveryone(helloAdaptor, []string{"Andy", "Bob", "Chris"})
-	runSayHelloContinuous(helloAdaptor, []string{"Andy", "Bob", "Chris"})
+	// runSayHelloContinuous(helloAdaptor, []string{"Andy", "Bob", "Chris"})
+	// runGetCurrentBalance(bankAdaptor, "7835697001xxxxx")
+	// runFetchExchangeRates(bankAdaptor, "USD", "JPN")
+	// runSummarizeTransactions(bankAdaptor, "7835697002yyyyy", 10)
+	runTransferMultiple(bankAdaptor, "7835697004", "7835697003", 200)
 }
 
 func runSayHello(adaptor *hello.HelloAdaptor, name string) {
@@ -52,4 +65,58 @@ func runSayHelloToEveryone(adaptor *hello.HelloAdaptor, names []string) {
 
 func runSayHelloContinuous(adaptor *hello.HelloAdaptor, names []string) {
 	adaptor.SayHelloContinuous(context.Background(), names)
+}
+
+func runGetCurrentBalance(adaptor *bank.BankAdapter, acct string) {
+	bal, err := adaptor.GetCurrentBalance(context.Background(), acct)
+
+	if err != nil {
+		log.Fatalln("Failed to call GetCurrentBalance :", err)
+	}
+
+	log.Println(bal)
+}
+
+func runFetchExchangeRates(adaptor *bank.BankAdapter, fromCur string, toCur string) {
+	adaptor.FetchExchangeRates(context.Background(), fromCur, toCur)
+}
+
+func runSummarizeTransactions(adaptor *bank.BankAdapter, acct string, numDummyTransactions int) {
+	var tx []dbank.Transaction
+
+	for i := 1; i <= numDummyTransactions; i++ {
+		ttype := dbank.TransactionTypeIn
+
+		if i%3 == 0 {
+			ttype = dbank.TransactionTypeOut
+		}
+
+		t := dbank.Transaction{
+			Amount:          float64(rand.Intn(500) + 10),
+			TransactionType: ttype,
+			Notes:           fmt.Sprintf("Dummy transaction %v", i),
+		}
+
+		tx = append(tx, t)
+	}
+
+	adaptor.SummarizeTransactions(context.Background(), acct, tx)
+}
+
+func runTransferMultiple(adaptor *bank.BankAdapter, fromAcct string, toAcct string,
+	numDummyTransactions int) {
+	var trf []dbank.TransferTransaction
+
+	for i := 1; i <= numDummyTransactions; i++ {
+		tr := dbank.TransferTransaction{
+			FromAccountNumber: fromAcct,
+			ToAccountNumber:   toAcct,
+			Currency:          "USD",
+			Amount:            float64(rand.Intn(200) + 5),
+		}
+
+		trf = append(trf, tr)
+	}
+
+	adaptor.TransferMultiple(context.Background(), trf)
 }
